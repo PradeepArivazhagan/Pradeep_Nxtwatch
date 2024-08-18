c390b5ee0be0d1a86558badd568706767cde0d16
PradeepArivazhagan <pradeepaagrib@gmail.com>
2024-08-18 05:40:38 +0000

    first commit

---
+++ src/context/ThemeAndVideoContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ThemeAndVideoContext = React.createContext({
+  isDark: false,
+  onChangeTheme: () => {},
+  onChangeTab: () => {},
+  activeTab: 'Home',
+  onSaveHandle: () => {},
+  savedVideos: [],
+})
+
+export default ThemeAndVideoContext
